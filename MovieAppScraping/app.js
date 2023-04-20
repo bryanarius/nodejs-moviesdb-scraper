9e3713056f975b48090a17aaad53d977de9fe127
Bryan Arius <98681192+Bryanarius@users.noreply.github.com>
2023-04-20 04:52:18 -0400

    removed hard coded data with webscrapped data

--- MovieAppScraping/app.js
+++ MovieAppScraping/app.js
@@ -14,27 +14,37 @@ async function scrapeData(url, page) {
     try {
         await page.goto(url, {waitUntil : 'load', timeout : 0})
         const html = await page.evaluate(() => document.body.innerHTML);
-        const $ = cheerio.load(html);
+        const $ = await cheerio.load(html);
 
+        let title = $('h2').text();
+        let releaseDate = $(".release_date").text();
+        let overview = $(".overview > p").text();
+        let userScore = $('.user_score_chart').attr('data-percent');
+        let imgUrl = $("#main > section > div.header.large.border.first.lazyloaded > div > div > section > div.poster > div.image_content > a > img").attr("src");
+        imgUrl = imgUrl.replaceWith('_filter(blur)', '')
+        browser.close()
 
         return {
-
+            title,
+            releaseDate,
+            overview,
+            userScore,
+            imgUrl
         }
+
     } catch(error) {
         console.log(error)
     }
 }
 
-async function getResults(req, res) {
-
-}
 app.get('/results', async function(req, res) {
     let url = req.query.search;
     browser = await puppeteer.launch({headless : false});
     const page = await browser.newPage();
 
     let data = await scrapeData(url,page);
-    browser.close()
+    res.render('results', {data:data});
+    
 });
 
 app.get('/search', (req,res)=>{
